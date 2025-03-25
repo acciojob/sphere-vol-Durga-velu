5af7ecd08a8c040987042ed478c29f17be5c8fd1
Durga-velu <durgadevi134babu@gmail.com>
2025-03-25 23:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function volume_sphere() {
     //Write your code here
 	let pi=3.14;
  let radius=Number(document.getElementById("radius").value)
- if(isNaN(radius.value) || radius.value<0 ){
+ if(isNaN(radius) || radius<0 ){
 	  document.getElementById("volume").textContent = NaN;
         return;
  }
 	
-let	volume= (4*pi*radius*radius*radius)/3
+ let volume = (4 * pi * Math.pow(radius, 3)) / 3;
 
 document.getElementById("volume").textContent=volume.toFixed(4)
 } 
