c91075d7ee0dd1dc74f2b60def0b791fe62763d7
Durga-velu <durgadevi134babu@gmail.com>
2025-03-25 23:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,5 @@ volume.value=volumeOfSphere.toFixed(4)
 
 } 
 
-window.onload =function () {
-document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload =document.getElementById('MyForm').onsubmit = volume_sphere;
 	
-} 
